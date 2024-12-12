fb103e8d0008674631d4aad1a6ebdc6bbea697e2
“Andrés” <andres.garcia36704@ucaldas.edu.co>
2024-12-12 00:03:02 -0500

    Proyecto Final web 2

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -3,7 +3,7 @@ import pgtools from "pgtools";
 
 const DB_NAME = process.env.DB_NAME || 'TechShop';
 const DB_USER = process.env.DB_USER || 'postgres';
-const DB_PASS = process.env.DB_PASSWORD || 'varusdracoon';
+const DB_PASS = process.env.DB_PASSWORD || '';
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || '5432';
 
